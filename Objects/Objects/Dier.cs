763f4a6a3405b70c9c52cb2d64d8867c676ec08a
Milan Van Cauwenberge <milan.vancauwenberge@viso-cor-mariae.be>
2024-01-22 11:54:03 +0100

    Layout zonder code

---
+++ Objects/Objects/Dier.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objects
+{
+    internal class Dier
+    {
+        private string soort;
+        private string naam;
+        private int aantalpoten;
+        private int leeftijd;
+    
+        public string Soort {  get { return soort; } set {  soort = value; } }
+        public string Naam {  get { return naam; } set { naam = value; } }
+        public int Aantalpoten { get {  return aantalpoten; } set {  aantalpoten = value; } }
+        public int Leeftijd { get {  return leeftijd; } set {  leeftijd = value; } }
+
+        public Dier(string soort, string naam, int aantalpoten, int leeftijd) 
+        {
+            this.soort = soort;
+            this.naam = naam;
+            this.aantalpoten = aantalpoten;
+            this.leeftijd = leeftijd;
+        }
+        public int Leeftijdplus(int leeftijdplus)
+        {
+            leeftijdplus = leeftijd += 1;
+
+            return leeftijdplus;
+        }
+    }
+}
---
+++ Objects/Objects/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Objects
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Dier Hond = new Dier("Hond", "Pablo", 4, 8);
+
+            Console.WriteLine("Hij is een " + Hond.Soort);
+            Console.WriteLine("Zijn naam is " + Hond.Naam);
+            Console.WriteLine("Hij heeft " + Hond.Aantalpoten + " poten");
+            Console.WriteLine("Hij is " + Hond.Leeftijd + " jaar oud");
+            Console.WriteLine($"Volgend jaar is Pablo {Hond.Leeftijdplus(Hond.Leeftijd)} jaar oud"); 
+        }
+    }
+}
